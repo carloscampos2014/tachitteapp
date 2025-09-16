7b2a248eb5386c034bbfd7ee08fe546083c6b9cf
Carlos Alberto de Lima Campos <carlos.campos.pro@prosoft.com.br>
2025-09-16 11:13:47 -0300

    teste: caminho da url base

--- TachittesApp/Program.cs
+++ TachittesApp/Program.cs
@@ -1,13 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TachittesApp;
-using TachittesApp.Services; // Adicione esta linha
+using TachittesApp.Services; 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-Console.WriteLine(builder.HostEnvironment.BaseAddress);
+Console.WriteLine($"Caminho Base:{builder.HostEnvironment.BaseAddress}");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton<CartService>(); // Adicione esta linha
+builder.Services.AddSingleton<CartService>(); 
 
 await builder.Build().RunAsync();
