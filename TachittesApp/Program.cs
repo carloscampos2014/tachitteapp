51bb0a65fdd21189d18dfec47d74fc95bcc79404
Carlos Alberto de Lima Campos <carlos.campos.pro@prosoft.com.br>
2025-09-16 11:04:23 -0300

    test:caminho base

--- TachittesApp/Program.cs
+++ TachittesApp/Program.cs
@@ -6,7 +6,7 @@ using TachittesApp.Services; // Adicione esta linha
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+Console.WriteLine(builder.HostEnvironment.BaseAddress);
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<CartService>(); // Adicione esta linha
 
